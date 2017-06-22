c215fd7306098b24598b8fe9ca63c6d2c7d10dfc
James Robert Perih <james@hotdang.ca>
2017-06-21 23:41:48 -0600

    Some view stuffs.

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const Papa = require('papaparse');
 const express = require('express');
+const os = require("os");
 const axios = require('axios');
 const pug = require('pug');
 const DEFAULT_URL = 'http://codefor.ca/wp-content/uploads/2017/06/dev-challenge-data.csv';
 
 let cachedParsedData = undefined;
+let cachedAnalysis = {};
 
 const app = express();
 app.set('views', './views');
 app.set('view engine', 'pug');
 
+/* initialize the app, fetching data */
 const init = (dataUrl) => {
   return new Promise(function(resolve, reject) {
     axios.get(dataUrl).then( function(response) {
@@ -24,21 +27,18 @@ const init = (dataUrl) => {
       reject(error);
     });
   });
-}
+};
 
-app.get('/', (req, res) => {
-  if (cachedParsedData) {
-    res.render('index', { data: cachedParsedData });
-  } else {
-    res.render('404');
-  };
-});
-
-app.get('/api/analyze/:type', (req, res) => {
+/* Analyze data by incident type */
+const analyzeType = (type) => {
   const analyzeStartTime = Date.now();
 
   if (!cachedParsedData) {
-    res.send('No data to analyze.');
+    return {};
+  }
+
+  if (cachedAnalysis[type]) {
+    return cachedAnalysis[type];
   }
 
   let incidentCount = 0;
@@ -46,7 +46,7 @@ app.get('/api/analyze/:type', (req, res) => {
   let lastIncident = 0;
 
   cachedParsedData.forEach((incident) => {
-    if (incident.violation_type === req.params.type) {
+    if (incident.violation_type === type) {
       const dateTimestamp = Date.parse(incident.violation_date);
 
       if (earliestIncident === 0) {
@@ -72,29 +72,81 @@ app.get('/api/analyze/:type', (req, res) => {
 
   // prepare output
   const results = {
-    incidentType: req.params.type,
     incidentCount: incidentCount,
     earliestIncident: earliestIncidentAsISO,
     latestIncident: lastIncidentAsISO
   };
+
+  cachedAnalysis[type] = results;
+  return cachedAnalysis[type];
+};
+
+/*
+  Path to view the page.
+ */
+app.get('/', (req, res) => {
+  if (cachedParsedData) {
+    res.render('index', { data: cachedParsedData });
+  } else {
+    res.render('404');
+  };
+});
+
+/*
+  Path to fetch the cached analysis data.
+  This data will change as the front-end user
+  continues to analyze unique incident types.
+ */
+app.get('/api/analyze/all', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
-  res.send(results);
+  res.send(cachedAnalysis);
+});
+
+/*
+  Path to fetch the analysis of an incident type.
+  If it is already cached, that is returned, otherwise
+  the analysis is conducted, cached, and returned.
+ */
+app.get('/api/analyze/:type', (req, res, next) => {
+  const type = req.params.type;
+
+  if (cachedAnalysis[type]) {
+    res.setHeader('Content-Type', 'application/json');
+    console.log(`Returned cached analytis for ${type}`);
+    res.send(cachedAnalysis[type]);
+  } else {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(analyzeType(type));
+  }
   // res.send(`${incidentCount} incidents of that type. Earliest on ${earliestIncidentAsISO}, last on ${lastIncidentAsISO}. Analysis took ${timeDifferenceInMillis}ms`);
 });
 
-app.get('/api/rawdata', (req, res) => {
+/*
+  Path to fetch the raw incident data.
+ */
+app.get('/api/rawdata', (req, res, next) => {
   if (!cachedParsedData) {
     res.setHeader('Content-Type', 'application/json');
     res.send({});
+    next();
   }
+
   res.setHeader('Content-Type', 'application/json');
   res.send(cachedParsedData);
 });
 
+/* Time to start! */
 init(process.argv.slice(2)[0] || DEFAULT_URL).then((response) => {
   cachedParsedData = response.data;
-  app.listen(3000, () => {
+  const PORT = 3000;
+  app.listen(PORT, () => {
     console.log(`${process.argv.slice(2)[0] ? 'Custom' : 'Default' } data cached and example app listening on port 3000!`);
+    console.log('');
+    console.log('Paths:');
+    console.log(`  http://${os.hostname()}:${PORT}/ - the front-end app.`);
+    console.log(`  http://${os.hostname()}:${PORT}/api/rawdata - JSON output of the raw data`);
+    console.log(`  http://${os.hostname()}:${PORT}/api/analyze/<type> - JSON output of analysis for specified type`);
+    console.log(`  http://${os.hostname()}:${PORT}/api/analyze/all - JSON output of cached analysis`);
   });
 }).catch((error) => {
   console.log(`Error getting data from URL specified: ${DATA_URL}`);
